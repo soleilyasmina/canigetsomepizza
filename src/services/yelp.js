e0405698beab8ea5677792296c9ac14107409034
soleilyasmina <mounlasolomon@gmail.com>
2021-07-13 14:20:03 -0400

    fix cors error

--- src/services/yelp.js
+++ src/services/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getPizzas = async (lat = 40.7290884, lon = -74.0037228) => {
   const resp = await axios({
     method: 'GET',
-    url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}&term=pizza&sort_by=distance&open_now=true`,
+    url: `https://cors-lite.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}&term=pizza&sort_by=distance&open_now=true`,
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_YELP_TOKEN}`,
     },
